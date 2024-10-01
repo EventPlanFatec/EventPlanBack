5f3f452b2cb53bfdf0d2ea888ad8e0ec27abd786
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-10-01 00:13:52 -0300

    Validação de Dados

--- EventPlanApp.Domain/Entities/UsuarioAdm.cs
+++ EventPlanApp.Domain/Entities/UsuarioAdm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,25 @@ namespace EventPlanApp.Domain.Entities
 {
     public class UsuarioAdm
     {
+        [Key]
         public int AdmId { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O email fornecido é inválido.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 100 caracteres.")]
         public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O nome de usuário é obrigatório.")]
+        [StringLength(50, ErrorMessage = "O nome de usuário não pode exceder 50 caracteres.")]
         public string NomeUsuario { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        [Phone(ErrorMessage = "O telefone fornecido é inválido.")]
         public string Telefone { get; set; }
 
         public ICollection<Organizacao> Organizacoes { get; set; }
     }
-
 }
