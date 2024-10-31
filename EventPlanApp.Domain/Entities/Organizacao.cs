faf8fb2c441ffc5c62326758c647229bd71bbcc9
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-10-30 23:27:40 -0300

    ImplementarEndPutOrganizations

--- EventPlanApp.Domain/Entities/Organizacao.cs
+++ EventPlanApp.Domain/Entities/Organizacao.cs
@@ -12,7 +12,7 @@ public class Organizacao
     public Organizacao(string cnpj, Endereco endereco, decimal notaMedia)
     {
         ValidateDomain(cnpj, notaMedia);
-        OrganizacaoId = new Random().Next(1, 1000); 
+        OrganizacaoId = new Random().Next(1, 1000);
         CNPJ = cnpj; 
         Endereco = endereco; 
         NotaMedia = notaMedia; 
@@ -27,5 +27,17 @@ public class Organizacao
 
         if (notaMedia < 0 || notaMedia > 10)
             throw new ArgumentException("A nota média deve estar entre 0 e 10.");
+    }
+    public void Update(string cnpj, Endereco endereco, decimal notaMedia)
+    {
+        if (string.IsNullOrWhiteSpace(cnpj) || cnpj.Length != 14)
+            throw new ArgumentException("O CNPJ deve ter 14 caracteres e é obrigatório.");
+
+        if (notaMedia < 0 || notaMedia > 10)
+            throw new ArgumentException("A nota média deve estar entre 0 e 10.");
+
+        CNPJ = cnpj;  // Atualiza o CNPJ
+        Endereco = endereco;  // Atualiza o Endereco
+        NotaMedia = notaMedia; // Atualiza a Nota Media
     }
 }
--- EventPlanApp.Domain/Interfaces/IOrganizacaoRepository.cs
+++ EventPlanApp.Domain/Interfaces/IOrganizacaoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,12 @@ namespace EventPlanApp.Domain.Interfaces
 {
     public interface IOrganizacaoRepository : IRepository<Organizacao>
     {
+        Task<IEnumerable<Organizacao>> FindAsync(Expression<Func<Organizacao, bool>> predicate);
         Task AddAsync(Organizacao organizacao);
-        
+        Task<Organizacao> GetByIdAsync(int id);
+        Task<IEnumerable<Organizacao>> GetAllAsync();
+        Task UpdateAsync(Organizacao entity);
+        Task DeleteAsync(int id);
+
     }
 }
