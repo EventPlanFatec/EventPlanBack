4fe2b9033760620831e74bbba9aa6f29b17d7014
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-11-13 16:57:09 -0300

    ImplemenAdicaoDeEventosAosFavoritos

---
+++ EventPlanApp.Api/Controllers/FavoritesController.cs
@@ -0,0 +1,41 @@
+﻿using EventPlanApp.Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlanApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FavoritesController : Controller
+    {
+        private readonly IFavoritesRepository _favoritesRepository;
+
+        public FavoritesController(IFavoritesRepository favoritesRepository)
+        {
+            _favoritesRepository = favoritesRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToFavorites(int eventId)
+        {
+            // Obtém o ID do usuário autenticado
+            var userId = User.FindFirst("userId")?.Value;
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            // Verifica se o evento já está na lista de favoritos do usuário
+            var isFavorited = await _favoritesRepository.IsEventFavoritedByUserAsync(userId, eventId);
+            if (isFavorited)
+            {
+                return BadRequest("Este evento já está nos favoritos.");
+            }
+
+            // Adiciona o evento aos favoritos
+            await _favoritesRepository.AddToFavoritesAsync(userId, eventId);
+            return Ok("Evento adicionado aos favoritos.");
+        }
+    }
+}
---
+++ EventPlanApp.Domain/Interfaces/IFavoritesRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanApp.Domain.Interfaces
+{
+    public interface IFavoritesRepository
+    {
+        Task<bool> IsEventFavoritedByUserAsync(string userId, int eventId);
+        Task AddToFavoritesAsync(string userId, int eventId);
+    }
+}
